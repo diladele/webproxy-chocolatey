05db48b24e356bd2b693920393d4b7822767354c
Rafael Akchurin <rafael.akchurin@diladele.com>
2021-05-04 14:14:40 +0200

    fix

--- src/webproxy/tools/chocolateyinstall.ps1
+++ src/webproxy/tools/chocolateyinstall.ps1
@@ -3,25 +3,17 @@ $ErrorActionPreference = 'Stop';
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $url        = 'https://packages.diladele.com/webproxy/1.1.0.E057/amd64/release/windows/webproxy-1.1.0.E057_amd64.msi'
 
-$WindowsVersion=[Environment]::OSVersion.Version
-if ($WindowsVersion.Major -ne "10") {
-  throw "This package requires Windows 10."
-}
-
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
   unzipLocation = $toolsDir
   fileType      = 'MSI'
   url           = $url
-  url64bit      = $url64
-
+  
   softwareName  = 'webproxy*'
 
   checksum      = 'AC1307725EFCFD3848D57BB5F4D0D0D7CD17B63EF61ADBD507D093123BAEBA92'
   checksumType  = 'sha256'
-  checksum64    = ''
-  checksumType64= 'sha256'
-
+  
   silentArgs    = "/qn /norestart /l*v `"$($env:TEMP)\$($packageName).$($env:chocolateyPackageVersion).MsiInstall.log`""
   validExitCodes= @(0, 3010, 1641)
 }
