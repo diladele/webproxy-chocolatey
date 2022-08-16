584770a167a4f7b41b2e4a670ee5594822d2582e
Rafael Akchurin <rafael.akchurin@diladele.com>
2022-08-16 17:47:52 +0200

    2.3

--- src/webproxy/tools/chocolateyinstall.ps1
+++ src/webproxy/tools/chocolateyinstall.ps1
@@ -1,7 +1,7 @@
 
 $ErrorActionPreference = 'Stop';
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url        = 'https://packages.diladele.com/webproxy/2.2.0.DB38/amd64/release/windows/webproxy-2.2.0.DB38_amd64.msi'
+$url        = 'https://packages.diladele.com/webproxy/2.3.0.C58B/amd64/release/windows/webproxy-2.3.0.C58B_amd64.msi'
 
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
@@ -11,7 +11,7 @@ $packageArgs = @{
   
   softwareName  = 'webproxy*'
 
-  checksum      = '3FFAE88DBD44D267D0C4C4B9084FD06AD68A8291D583BF2491A80CE9C3E7939B'
+  checksum      = '089D7FA6B13ED5E7C943BCAB24F1DD4FD06C155C7FDFD6A28A163188DEC2BBE6'
   checksumType  = 'sha256'
   
   silentArgs    = "/qn /norestart /l*v `"$($env:TEMP)\$($packageName).$($env:chocolateyPackageVersion).MsiInstall.log`""
